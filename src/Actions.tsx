2140e943cc601ef10cda1997204af650f59a1d8d
Jon <jonegeland@gmail.com>
2020-09-06 18:01:57 -0700

    more clean up

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -1,5 +1,3 @@
-import _ from "lodash";
-
 export const ActionTypes = {};
 
 export type Action = {
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-import "./style/reset.css";
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
