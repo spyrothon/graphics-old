845a1757dac6488c533ad5a3b6ee75cfec0b816c
Jon <jonegeland@gmail.com>
2020-09-11 15:20:19 -0700

    store SVG filters in a Library

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,11 @@ import { Provider } from "react-redux";
 import { store } from "./Store";
 
 import Standard1 from "./layouts/standard/Standard1";
+import SVGLibrary from "./uikit/svg/SVGLibrary";
 
 ReactDOM.render(
   <Provider store={store}>
+    <SVGLibrary />
     <Standard1 />
   </Provider>,
   document.querySelector("#app-container"),
