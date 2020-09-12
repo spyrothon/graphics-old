7e1b8269403c3cdfcfe29e49654c149465a3b441
Jon <jonegeland@gmail.com>
2020-09-11 22:55:30 -0700

    Some kind of Paneling for the dashboard

--- src/dashboards/AdminDashboard.tsx
+++ src/dashboards/AdminDashboard.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
+
 import RemoteSyncSenderManager from "../modules/remote/RemoteSyncSenderManager";
 import { updateGameName } from "../modules/run-info/RunInfoActions";
 import TextInput from "./uikit/TextInput";
 import Button from "./uikit/Button";
+import Panel from "./uikit/Panel";
+import Header from "./uikit/Header";
+
+import styles from "./AdminDashboard.mod.css";
 
 export default function AdminDashboard() {
   const [gameName, setGameName] = React.useState("");
@@ -12,9 +17,14 @@ export default function AdminDashboard() {
   }
 
   return (
-    <div>
-      <TextInput label="Game Name" onChange={(event) => setGameName(event.target.value)} />
-      <Button onClick={() => sendUpdate()}>Set Game Name</Button>
+    <div className={styles.container}>
+      <Header size={Header.Sizes.H1}>Graphics Dashboard</Header>
+      <div className={styles.mosaic}>
+        <Panel name="Run Info" className={styles.panelRunInfo}>
+          <TextInput label="Game Name" onChange={(event) => setGameName(event.target.value)} />
+          <Button onClick={() => sendUpdate()}>Set Game Name</Button>
+        </Panel>
+      </div>
     </div>
   );
 }
