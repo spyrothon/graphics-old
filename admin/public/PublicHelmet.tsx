34a56f4be4df46b69fe661dd46de907d415f79d3
Jon <jonegeland@gmail.com>
2020-11-01 12:59:06 -0800

    proper page title

--- admin/public/PublicHelmet.tsx
+++ admin/public/PublicHelmet.tsx
@@ -9,6 +9,7 @@ export default function PublicHelmet(props: PublicHelmetProps) {
   const { className } = props;
   return (
     <Helmet>
+      <title>Spyrothon 6 | November 6-8, 2020</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className={className} />
     </Helmet>
