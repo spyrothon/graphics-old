adf9123465f8312c10c6ccec496fa5b6990af456
Jon <jonegeland@gmail.com>
2020-11-02 17:22:13 -0800

    prod server update

--- server/index.js
+++ server/index.js
@@ -6,15 +6,17 @@ const cors = require("cors");
 const app = express();
 
 const {
-  private: { HOSTNAME, PORT },
+  private: {
+    admin: { HOSTNAME, PORT },
+  },
 } = require("../config/production.json");
 
 app.use(cors());
 app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/admin")));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, "../public") });
+  res.sendFile("index.html", { root: path.join(__dirname, "../public/admin") });
 });
 
 app.listen(PORT, HOSTNAME, function () {
