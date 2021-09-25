c2e2b93b92fd81976e7e0b5196fd476009a0a31a
Jon <jonegeland@gmail.com>
2021-09-25 00:30:57 -0400

    fix countdown timer

--- graphics/layouts/breaks/BreakCountdown.tsx
+++ graphics/layouts/breaks/BreakCountdown.tsx
@@ -1,18 +1,43 @@
 import * as React from "react";
 
+import { Schedule } from "../../../api/APITypes";
+import { useSafeSelector } from "../../Store";
+import useAnimationFrame from "../../hooks/useAnimationFrame";
+import ScheduleStore from "../../modules/schedules/ScheduleStore";
+import DurationUtils from "../../modules/time/DurationUtils";
 import Layout from "../../uikit/Layout";
 
 import styles from "./Break.mod.css";
 
-import logo from "../../res/spyrothon_logo.png";
+function getRemainingSeconds(startTime: Date) {
+  return (new Date().getTime() - startTime.getTime()) / 1000;
+}
+
+function CountdownTimer({ schedule }: { schedule: Schedule }) {
+  const { startTime } = schedule;
+  const [time, setTime] = React.useState(() => getRemainingSeconds(startTime));
+
+  useAnimationFrame(() => {
+    return setTime(getRemainingSeconds(startTime));
+  }, [startTime]);
+
+  return <>{DurationUtils.toString(time)}</>;
+}
 
 export default function BreakCountdown() {
+  const schedule = useSafeSelector(ScheduleStore.getSchedule);
+
+  if (schedule == null) return null;
+
   return (
     <Layout showBackground={false}>
       <div className={styles.breakCountdown}>
-        <img className={styles.logo} src={logo} />
+        <img className={styles.logo} src={schedule.logoUrl} />
         <div className={styles.backSoon}>
           <div className={styles.backSub}>Starting soon...</div>
+          <div className={styles.backHeader}>
+            <CountdownTimer schedule={schedule} />
+          </div>
         </div>
       </div>
     </Layout>
