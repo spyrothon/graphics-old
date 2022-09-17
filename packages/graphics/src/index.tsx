d4760ed96e21139312cc26cd126cacf54350b262
faultyserver <jonegeland@gmail.com>
2022-09-16 20:17:10 -0400

    move res back to src

--- packages/graphics/src/index.tsx
+++ packages/graphics/src/index.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import { store } from "./Store";
 import ThemeProvider from "./uikit/ThemeProvider";
 
-ReactDOM.render(
+const root = createRoot(document.querySelector("#app-container")!);
+
+root.render(
   <Provider store={store}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
   </Provider>,
-  document.querySelector("#app-container"),
 );
