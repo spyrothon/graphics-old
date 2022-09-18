43c8885d221d99b110578965112e10e87eca1d0e
faultyserver <jonegeland@gmail.com>
2022-09-18 04:33:25 -0400

    Fix dependency resolution problem because workspace.yaml was misnamed

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -4,11 +4,12 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-// import "@spyrothon/uikit/style.css";
 import { ThemeProvider } from "@spyrothon/uikit";
 
 import App from "./App";
 import { store } from "./Store";
+
+import "@spyrothon/uikit/style.css";
 
 const root = createRoot(document.querySelector("#app-container")!);
 
